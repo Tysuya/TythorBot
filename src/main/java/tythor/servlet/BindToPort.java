373fdb7031e15320b67e78adbba8105a176907a7
Tythor <tthorin@live.com>
2017-01-19 20:49:56 -0800

    TythorBot

--- src/main/java/tythor/servlet/BindToPort.java
+++ src/main/java/tythor/servlet/BindToPort.java
@@ -4,7 +4,11 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by Tyler on 1/19/2017.
@@ -18,5 +22,10 @@ public class BindToPort extends HttpServlet {
         context.addServlet(new ServletHolder(new BindToPort()), "/*");
         server.start();
         server.join();
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.getWriter().print("Hello from Tyler!\n");
     }
 }
