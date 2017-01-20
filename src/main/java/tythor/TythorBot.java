9c7eb1f0351258e1226b74ace5d3e2cb1f39b7f7
Tythor <tthorin@live.com>
2017-01-19 20:39:18 -0800

    TythorBot

--- src/main/java/tythor/TythorBot.java
+++ src/main/java/tythor/TythorBot.java
@@ -10,12 +10,6 @@ import static tythor.servlet.BindToPort.bindToPort;
 public class TythorBot {
     public static JDA jda;
     public static void main(String[] args) {
-        try {
-            bindToPort();
-        } catch(Exception e) {
-            e.printStackTrace();
-        }
-
         JDABuilder jdaBuilder = new JDABuilder(AccountType.BOT).setToken("MjcwMTEwMjEzNTIzMTExOTM2.C1zHQA.geSuOqz1FtfiMU86GOmRoiGPmOs");
                 // MTU5MjAxNTI2MTE0NTQ5NzYw.C1kPQw.eT99T2xS8VjmTcWzOgyB8m3gg7I
 
@@ -23,6 +17,11 @@ public class TythorBot {
         try {
             jda = jdaBuilder.buildBlocking();
             jda.addEventListener(new MessageListener());  //An instance of a class that will handle events.
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            bindToPort();
         } catch(Exception e) {
             e.printStackTrace();
         }
