49896bf7a0ce6cf845793867872e08c3620e96a4
Tythor <tthorin@live.com>
2017-01-31 01:49:18 -0800

    TythorBot

--- src/main/java/tythor/commands/Uptime.java
+++ src/main/java/tythor/commands/Uptime.java
@@ -13,7 +13,7 @@ public class Uptime {
         long seconds = time % 60;
         long minutes = time / 60 % 60;
         long hours = time / 3600 % 24;
-        long days = time / 86400 % 7;
+        long days = time / 86400;
         String secondsString = " seconds ";
         String minutesString = " minutes ";
         String hoursString = " hours ";
@@ -24,125 +24,10 @@ public class Uptime {
             minutesString = " minute ";
         if(hours == 1)
             hoursString = " hour ";
-        if(hours == 1)
+        if(days == 1)
             daysString = " day ";
 
-        channel.sendMessage("TythorBot has been online for ```" + days + daysString + hours + hoursString + minutes + minutesString + seconds + secondsString + "```").queue();
+        channel.sendMessage("TythorBot has been online for```" + days + daysString + hours + hoursString + minutes + minutesString + seconds + secondsString + "```").queue();
     }
-    public static void uptime2(MessageChannel channel) {
-        long time = (System.currentTimeMillis() - TythorBot.uptime) / 1000;
-        System.out.println("time: " + time);
-        String period = "seconds";
-        if(time >= 604800) {
-            String leftover = " ";
-            long tempTime = time / 86400 % 7;
-            if(tempTime == 0)
-                leftover = leftover;
-            if(tempTime == 1)
-                leftover += Long.toString(tempTime).toString() + " day ";
-            else
-                leftover += Long.toString(tempTime).toString() + " days ";
 
-            tempTime = time / 3600 % 24;
-            if(tempTime == 0)
-                leftover = leftover;
-            if(tempTime == 1)
-                leftover += Long.toString(tempTime).toString() + " hour ";
-            else
-                leftover += Long.toString(tempTime).toString() + " hours ";
-
-            tempTime = time / 60 % 60;
-            if(tempTime == 0)
-                leftover = leftover;
-            else if(tempTime == 1)
-                leftover += Long.toString(tempTime).toString() + " minute ";
-            else
-                leftover += Long.toString(tempTime).toString() + " minutes ";
-
-            tempTime = time % 60;
-            if(tempTime == 0)
-                leftover = leftover;
-            else if(tempTime == 1)
-                leftover += Long.toString(tempTime).toString() + " second";
-            else
-                leftover += Long.toString(tempTime).toString() + " seconds";
-
-            time /= 604800;
-            if(time == 1)
-                period = "week" + leftover;
-            else
-                period = "weeks" + leftover;
-        } else if(time >= 86400) {
-            String leftover = " ";
-            long tempTime = time / 3600 % 24;
-            if(tempTime == 0)
-                leftover = leftover;
-            if(tempTime == 1)
-                leftover += Long.toString(tempTime).toString() + " hour ";
-            else
-                leftover += Long.toString(tempTime).toString() + " hours ";
-
-            tempTime = time / 60 % 60;
-            if(tempTime == 0)
-                leftover = leftover;
-            else if(tempTime == 1)
-                leftover += Long.toString(tempTime).toString() + " minute ";
-            else
-                leftover += Long.toString(tempTime).toString() + " minutes ";
-
-            tempTime = time % 60;
-            if(tempTime == 0)
-                leftover = leftover;
-            else if(tempTime == 1)
-                leftover += Long.toString(tempTime).toString() + " second";
-            else
-                leftover += Long.toString(tempTime).toString() + " seconds";
-
-            time /= 86400;
-            if(time == 1)
-                period = "day" + leftover;
-            else
-                period = "days" + leftover;
-        } else if(time >= 3600) {
-            String leftover = " ";
-            long tempTime = time / 60 % 60;
-            if(tempTime == 0)
-                leftover = leftover;
-            else if(tempTime == 1)
-                leftover += Long.toString(tempTime).toString() + " minute ";
-            else
-                leftover += Long.toString(tempTime).toString() + " minutes ";
-            System.out.println(tempTime);
-
-            tempTime = time % 60;
-            if(tempTime == 0)
-                leftover = leftover;
-            else if(tempTime == 1)
-                leftover += Long.toString(tempTime).toString() + " second";
-            else
-                leftover += Long.toString(tempTime).toString() + " second";
-
-            time /= 3600;
-            if(time == 1)
-                period = "hour" + leftover;
-            else
-                period = "hours" + leftover;
-        } else if(time >= 60) {
-            String leftover = " ";
-            long tempTime = time % 60;
-            if(tempTime == 0)
-                leftover = leftover;
-            else if(tempTime == 1)
-                leftover += Long.toString(tempTime).toString() + " second";
-            else
-                leftover += Long.toString(tempTime).toString() + " seconds";
-
-            time /= 60;
-            if(time == 1)
-                period = "minute" + leftover;
-            else
-                period = "minutes" + leftover;
-        }
-        channel.sendMessage(Long.toString(time) + " " + period).queue();
-    }
 }
